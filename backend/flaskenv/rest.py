1b33299fb8e8632a87d8ef99ba9f3d3586f5c264
Alexander Kumamoto <kumamotoalex@berkeley.edu>
2015-10-10 14:54:36 -0700

    REST update with linalg

--- backend/flaskenv/rest.py
+++ backend/flaskenv/rest.py
@@ -1,25 +1,44 @@
+#!flask/bin/python
 from flask import Flask, request, jsonify
+import numpy as np
 
 rest = Flask(__name__)
-NUM_MOVIES = 
-NUM_CATEGORIES = 
-PREFERENCE_MATRIX = 
+NUM_MOVIES = 5
+NUM_CATEGORIES = 5
+PREFERENCE_MATRIX = np.identity(5)
 
-# CREATE USER
-@rest.route('/makeuser', methods = ['POST'])
-def create_user():
+# # CREATE USER - passes in user_name, URL of image
+# @rest.route('/makeuser', methods = ['POST'])
+# def create_user():
+# 	return
 
-# SEND PREFERENCES
-@rest.route('/sendscore', methods = ['POST'])
-def send_preferences():
 
-# GET PREFERENCES
-@rest.route('/getpreferences/<int:user_id>', methods = ['GET'])
-def get_preferences():
+# # SEND PREFERENCES
+# @rest.route('/sendscore', methods = ['POST'])
+# def send_preferences():
+# 	return
 
-# GET MATCHES
-@rest.route('/getmatches/<int:user_id>', methods = ['GET'])
-def get_matches():
+# # GET PREFERENCES - return URL
+@rest.route('/getpreferences/<string:user_name>', methods = ['GET'])
+def get_preferences(user_name):
+	# QUERY DATABASE
 
-if __name__ == 'main':
+	return 
+
+# # GET MATCHES
+@rest.route('/getmatches/<string:user_name>', methods = ['GET'])
+def get_matches(user_name):
+	# QUERY DATABASE
+	r = calculate_score([1,0,0,0,0])
+	return str(r).strip('[]')
+
+#Takes in a score list and returns the array of preferences - the 'preference vetor'
+def calculate_score(score_list):
+	invect = np.array(score_list)
+	outvect = PREFERENCE_MATRIX.dot(invect)
+	return outvect.tolist()
+
+
+
+if __name__ == '__main__':
 	rest.run(debug=True)
