cfb750e2134c892af9e75c2b84643d96596c1a0f
Kevin Jiao <kevin.jiao@berkeley.edu>
2015-10-11 09:44:52 -0700

    YEAAA

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Avatar from 'material-ui/lib/avatar';
+
 export default class Navbar extends Component {
   render() {
+    var split = document.URL.split('/');
+    var name = split[split.length-1];
+    var imgurl = "../img/" + name + ".jpg";
+
       return (
       <nav>
         <div className="nav-wrapper">
+          <Avatar src = {imgurl}/>
+          <span style={{fontSize:'150%'}}> {name}</span>
           <a href="#" className="brand-logo center"><span style={{ fontFamily: 'Playfair Display', fontSize: '200%'}}>Flix</span></a>
           <ul id="nav-mobile" className="left hide-on-med-and-down">
           </ul>
