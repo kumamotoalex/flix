b1a9dedc632e42d6824819790b142779b5e8d960
Alexander Kumamoto <kumamotoalex@berkeley.edu>
2015-10-11 03:19:22 -0700

    fix indented block

--- backend/flaskenv/prefmatrix.py
+++ backend/flaskenv/prefmatrix.py
@@ -3,7 +3,7 @@ import numpy as np
 # Returns the preferance matrix
 def generate_prefmatrix():
 # 		Forrest_Gump | Frozen | Star_Wars | Parent_Trap | The_Notebook | Harry_Potter | Bat_Man | Finding_Nemo | The_Hangover | Inception
-return np.array([
+	return np.array([
 		[1,				0,		0,			1,				1,			0,				0,			0,			0,				0], #Drama
 		[0,				1,		0,			0,				1,			1,				0,			1,			0,				0], #Family
 		[0,				0,		1,			0,				0,			1,				1,			1,			0,				1], #Adventure
